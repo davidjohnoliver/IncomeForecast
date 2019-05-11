9d0d17a3d98b8a495efe4ac271aba03332b02422
Dissolute <david.john.oliver@gmail.com>
2019-05-11 17:19:51 -0400

    Tax - remove last year's brackets

--- tax.py
+++ tax.py
@@ -28,23 +28,6 @@ _caBrackets = [
     _TaxBracket(210371.01, 0, 33),
 ]
 
-""" _qcBrackets = [ #2017
-    _TaxBracket(0, 15012, 0),
-    _TaxBracket(15012.01, 43055, 15),
-    _TaxBracket(43055.01, 86105, 20),
-    _TaxBracket(86105.01, 104765, 24),
-    _TaxBracket(104765.01, 0, 25.75),
-]
-
-_caBrackets = [
-    _TaxBracket(0, 11809, 0),
-    _TaxBracket(11809.01, 46605, 15),
-    _TaxBracket(46605.01, 93208, 20.5),
-    _TaxBracket(93208.01, 144489, 26),
-    _TaxBracket(144489.01, 205842, 29),
-    _TaxBracket(205842.01, 0, 33),
-] """
-
 _qcAbatement = 16.5 # https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/deductions-credits-expenses/line-440-refundable-quebec-abatement.html
 
 def get_income_tax_from_brackets(taxable_income : float, abatement : float, brackets : List[_TaxBracket]):
