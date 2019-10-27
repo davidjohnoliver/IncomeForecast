1e003ecb65986c201f8d683d5d20ffcbf678fe72
David Oliver <david.john.oliver@gmail.com>
2019-10-27 15:55:18 -0400

    Add first complete ruleset
    This pulls together the rules created thus far into a complete simulation description.

---
+++ rulesets.py
@@ -0,0 +1,43 @@
+"""
+Defined model rulesets.
+"""
+import ruleset
+import salary_rules
+import savings_rules
+import spending_rules
+
+def v1(
+    salary_compound_rate : float, 
+    salary_plateau : float, 
+    base_spending : float, 
+    spending_luxury_compound_rate : float, 
+    initial_rrsp_allotment : float, 
+    final_rrsp_allotment : float, 
+    initial_year : int, 
+    year_of_retirement : int, 
+    year_of_death : int,
+    retirement_income : float,
+    rrsp_interest_rate : float,
+    tfsa_interest_rate : float
+    ):
+    """
+    A complete ruleset based on simple, plausible models for spending and income growth.
+
+    See the individual rules for an explanation of the input parameters.
+    """
+    career_length_yrs = year_of_retirement - initial_year
+    return (
+        ruleset.get_career_rules(
+            salary_rules.get_compound_plateau(salary_compound_rate, salary_plateau),
+            spending_rules.get_luxury_over_basic(base_spending, spending_luxury_compound_rate),
+            savings_rules.get_simple_linear(initial_rrsp_allotment, final_rrsp_allotment, initial_year, career_length_yrs),
+            rrsp_interest_rate,
+            tfsa_interest_rate
+        ),
+        ruleset.get_retirement_rules(
+            retirement_income,
+            savings_rules.get_simple_retirement_deduction(year_of_retirement, year_of_death),
+            rrsp_interest_rate,
+            tfsa_interest_rate
+        )
+    )
---
+++ tests/test_rulesets.py
@@ -0,0 +1,40 @@
+import rulesets
+import sim
+import solve
+
+def test_v1_runs():
+    """
+    This is a minimal sanity-test that using the ruleset doesn't throw any errors. It doesn't verify correctness (which isn't really possible for full rulesets).
+    """
+
+    simulation = sim.Simulation()
+    simulation.age_at_retirement = 60
+    simulation.year_of_birth = 1990
+    simulation.initial_year = 2025
+    simulation.age_at_death = 80
+    simulation.savings_at_death = 10000
+    simulation.initial_salary = 40000
+    simulation._initial_savings_rrsp = 5000
+    simulation.initial_savings_tfsa = 600
+
+    simulation.set_solver(solve.binary_solver)
+
+    career_rules, retirement_rules = rulesets.v1(
+        salary_compound_rate=0.05,
+        salary_plateau=70000,
+        base_spending=30000,
+        spending_luxury_compound_rate=0.04,
+        initial_rrsp_allotment=0.5,
+        final_rrsp_allotment=0.5,
+        initial_year=simulation.initial_year,
+        year_of_retirement=simulation.year_of_retirement,
+        year_of_death=simulation.year_of_death,
+        retirement_income=50000,
+        rrsp_interest_rate=0.05,
+        tfsa_interest_rate=0.05
+    )
+
+    simulation.set_rules(career_rules)
+    simulation.set_retirement_rules(retirement_rules)
+
+    simulation.run()
