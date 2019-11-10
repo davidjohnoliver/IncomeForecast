62f96ca249578075d8ad9e2d2541029935b1986e
David Oliver <david.john.oliver@gmail.com>
2019-11-10 14:27:13 -0500

    Add Bose ruleset
    Swaps in the capped spending rule.

--- rulesets.py
+++ rulesets.py
@@ -41,3 +41,40 @@ def ampere(
             tfsa_interest_rate
         )
     )
+
+def bose(
+    salary_compound_rate : float, 
+    salary_plateau : float, 
+    base_spending : float, 
+    spending_luxury_compound_rate : float, 
+    cap_fractional : float,
+    initial_rrsp_allotment : float, 
+    final_rrsp_allotment : float, 
+    initial_year : int, 
+    year_of_retirement : int, 
+    year_of_death : int,
+    retirement_income : float,
+    rrsp_interest_rate : float,
+    tfsa_interest_rate : float
+    ):
+    """
+    A complete ruleset based on simple, plausible models for spending and income growth. Spending is capped to some fraction of disposable income.
+
+    See the individual rules for an explanation of the input parameters.
+    """
+    career_length_yrs = year_of_retirement - initial_year
+    return (
+        ruleset.get_career_rules(
+            salary_rules.get_compound_plateau(salary_compound_rate, salary_plateau),
+            spending_rules.get_luxury_over_basic_capped(base_spending, spending_luxury_compound_rate, cap_fractional),
+            savings_rules.get_simple_linear(initial_rrsp_allotment, final_rrsp_allotment, initial_year, career_length_yrs),
+            rrsp_interest_rate,
+            tfsa_interest_rate
+        ),
+        ruleset.get_retirement_rules(
+            retirement_income,
+            savings_rules.get_simple_retirement_deduction(year_of_retirement, year_of_death),
+            rrsp_interest_rate,
+            tfsa_interest_rate
+        )
+    )
--- tests/test_rulesets.py
+++ tests/test_rulesets.py
@@ -2,7 +2,7 @@ import rulesets
 import sim
 import solve
 
-def test_v1_runs():
+def test_ampere_runs():
     """
     This is a minimal sanity-test that using the ruleset doesn't throw any errors. It doesn't verify correctness (which isn't really possible for full rulesets).
     """
@@ -38,3 +38,47 @@ def test_v1_runs():
     simulation.set_retirement_rules(retirement_rules)
 
     simulation.run()
+
+    assert 46 == len(simulation.all_deltas)
+    assert 46 == len(simulation.all_funds)
+
+def test_bose_runs():
+    """
+    This is a minimal sanity-test that using the ruleset doesn't throw any errors. It doesn't verify correctness (which isn't really possible for full rulesets).
+    """
+
+    simulation = sim.Simulation()
+    simulation.age_at_retirement = 60
+    simulation.year_of_birth = 1990
+    simulation.initial_year = 2025
+    simulation.age_at_death = 80
+    simulation.savings_at_death = 10000
+    simulation.initial_salary = 40000
+    simulation._initial_savings_rrsp = 5000
+    simulation.initial_savings_tfsa = 600
+
+    simulation.set_solver(solve.binary_solver)
+
+    career_rules, retirement_rules = rulesets.bose(
+        salary_compound_rate=0.05,
+        salary_plateau=70000,
+        base_spending=30000,
+        spending_luxury_compound_rate=0.04,
+        cap_fractional=0.9,
+        initial_rrsp_allotment=0.5,
+        final_rrsp_allotment=0.5,
+        initial_year=simulation.initial_year,
+        year_of_retirement=simulation.year_of_retirement,
+        year_of_death=simulation.year_of_death,
+        retirement_income=50000,
+        rrsp_interest_rate=0.05,
+        tfsa_interest_rate=0.05
+    )
+
+    simulation.set_rules(career_rules)
+    simulation.set_retirement_rules(retirement_rules)
+
+    simulation.run()
+
+    assert 46 == len(simulation.all_deltas)
+    assert 46 == len(simulation.all_funds)
