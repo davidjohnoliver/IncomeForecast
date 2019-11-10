cce22abb25de6ba0ed000f6e057896067f32fa21
David Oliver <david.john.oliver@gmail.com>
2019-11-10 13:59:50 -0500

    Add capped spending rule
    Caps spending to stay under net income, to give solutions that avoid unrealistic behaviours like late-career negative savings.

--- spending_rules.py
+++ spending_rules.py
@@ -24,3 +24,25 @@ def get_luxury_over_basic(base_spending: float, luxury_compound_rate: float):
         return deltas.update_spending(base_spending + new_luxury)
 
     return luxury_over_basic
+
+def get_luxury_over_basic_capped(base_spending: float, luxury_compound_rate: float, cap_fractional: float):
+    """
+    Models expenditure as a compounding 'luxury' component added to a fixed 'basic needs' component. Caps spending at some fraction of net income. 
+    
+    sp[y] = min(b + (1 + c)sp[y-1], f * i), where sp = spending, b = base_spending, c = luxury_compound_rate, f = cap_fractional, i = deltas.total_net_income
+    """
+
+    def luxury_over_basic_capped(deltas: model.deltas_state, previous_funds: model.funds_state, previous_deltas: model.deltas_state):
+        previous_luxury = previous_deltas.spending - base_spending
+        if (previous_luxury < 0):
+                # Try to somehow handle spending below the 'basic level,' since the solver will try spendings from 0 upwards. Consumer code 
+                # is responsible for warning the user if the actual solution returns a spending below the 'basic' level.
+                new_spending = (1 + luxury_compound_rate) * previous_deltas.spending
+                return deltas.update_spending(new_spending)
+        
+        new_luxury = (1 + luxury_compound_rate) * previous_luxury
+        cap = deltas.total_net_income * cap_fractional
+        spending = min(cap, base_spending +  new_luxury)
+        return deltas.update_spending(spending)
+
+    return luxury_over_basic_capped
--- tests/test_spending_rules.py
+++ tests/test_spending_rules.py
@@ -7,7 +7,21 @@ def standard_previous_deltas():
 def standard_previous_funds():
     return model.funds_state(0, 0, 1999)
 
-def test_simple_increase():
+def test_get_luxury_over_basic():
     rule = spending_rules.get_luxury_over_basic(20000, 0.05)
-    new_delta = model.get_updated_deltas_from_rules(standard_previous_funds(), standard_previous_deltas().update_spending(50000), {rule})
+    new_delta = model.get_updated_deltas_from_rules(standard_previous_funds(), standard_previous_deltas().update_spending(50000), [rule])
     assert 51500 == new_delta.spending
+
+def test_get_luxury_over_basic_capped_below():
+    rule = spending_rules.get_luxury_over_basic_capped(20000, 0.05, 0.9)
+    def set_salary(deltas: model.deltas_state, previous_funds: model.funds_state, previous_deltas: model.deltas_state):
+        return deltas.update_gross_salary(100000)
+    new_delta = model.get_updated_deltas_from_rules(standard_previous_funds(), standard_previous_deltas().update_spending(50000), [set_salary, rule])
+    assert 51500 == new_delta.spending
+
+def test_get_luxury_over_basic_capped_above():
+    rule = spending_rules.get_luxury_over_basic_capped(20000, 0.05, 0.9)
+    def set_salary(deltas: model.deltas_state, previous_funds: model.funds_state, previous_deltas: model.deltas_state):
+        return deltas.update_gross_salary(55800)
+    new_delta = model.get_updated_deltas_from_rules(standard_previous_funds(), standard_previous_deltas().update_spending(50000), [set_salary, rule])
+    assert 50220 == new_delta.spending
