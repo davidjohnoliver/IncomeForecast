1ed4ad8a4f9dfc9b3c1b859e11087978ba446f39
David Oliver <david.john.oliver@gmail.com>
2019-12-05 22:07:09 -0500

    Add get_increasing_savings_increasing_spending() rule
    This is a somewhat more complicated rule that has been designed to exhibit common-sense expected behaviour (proportional savings should only ever increase, absolute spending should only ever increase, as long as salary is increasing or constant), while maximising 'solvability' (very high or low initial spending inputs should lead to spending almost all or almost none of lifetime after-tax earnings, giving the best possible chance of finding a solution for a given savings target).

--- spending_rules.py
+++ spending_rules.py
@@ -4,7 +4,6 @@ Rules describing the trajectory of spending over time.
 
 import model
 
-
 def get_luxury_over_basic(base_spending: float, luxury_compound_rate: float):
     """
     Models expenditure as a compounding 'luxury' component added to a fixed 'basic needs' component.
@@ -46,3 +45,73 @@ def get_luxury_over_basic_capped(base_spending: float, luxury_compound_rate: flo
         return deltas.update_spending(spending)
 
     return luxury_over_basic_capped
+
+def get_increasing_savings_increasing_spending(initial_year : float, increase_savings_weight : float):
+    """
+    A somewhat complex spending rule which tries to enforce the following intuitively desirable traits:
+        - savings, as a fraction of after-tax income, should only ever increase
+        - spending, in absolute dollars, should only ever increase
+    
+    It also has the following mathematically useful characteristics:
+        - if the initial savings is 0 (ie initial spending == initial after-tax income), then total career savings will be very close to 0
+        - if the initial spending is 0, then total career spending will be very close to 0
+    (These characteristics maximise the ability to find a solution for a given income function and savings target)
+
+    It determines the spending per-year as follows:
+        1. The spending that would maintain the same proportional savings is taken as a ceiling, call this sp_ceil
+        2. The spending that would maintain the same absolute spending (ie the previous year's value) is taken as sp_floor
+        3. Actual spending is sp = sp_ceil*(1 - increase_savings_weight) + sp_floor*increase_savings_weight.
+            Hence if increase_savings_weight = 0.5, the average will be taken. If increase_savings_weight = 1, any increase in salary will be
+            entirely put into savings. If increase_savings_weight = 0, any increase in salary will be entirely spent. 
+        4. Spending will be clamped to be no greater than after-tax income.
+
+        0. Actually, in order to have the aforementioned mathematically-useful character of 'maxing out' or 'zeroing out' total career spending,
+            the requested increase_savings_weight will actually be tweaked if the initial spending is at 95% or 5% of initial after-tax income,
+            to ensure that spending is indeed maximized or minimized.
+    
+    """
+    
+    actual_increase_savings_weight = increase_savings_weight
+
+    def increasing_savings_increasing_spending(deltas: model.deltas_state, previous_funds: model.funds_state, previous_deltas: model.deltas_state):
+        nonlocal actual_increase_savings_weight
+        if previous_deltas.year == initial_year:
+            # This is the first year, adjust increase_savings_weight if need be. If spending is in the upper edge region, savings weight 
+            # should be decreased to push spending still higher. If it's in the lower edge region, it should be increased to push it still lower.
+            # 
+            initial_spending_fraction = previous_deltas.spending / previous_deltas.total_net_income
+            initial_spending_fraction = min(initial_spending_fraction, 1) # Clamp to avoid weirdness
+            EDGE_REGION = 0.05
+            actual_increase_savings_weight = get_maxed_or_zeroed_out(1 - initial_spending_fraction, increase_savings_weight, EDGE_REGION)
+            
+        previous_savings_absolute = previous_deltas.total_net_income - previous_deltas.spending
+        previous_savings_proportional = previous_savings_absolute / previous_deltas.total_net_income
+        new_savings_proportional = previous_savings_proportional
+        new_savings_for_ceiling = new_savings_proportional * deltas.total_net_income
+        sp_ceiling = deltas.total_net_income - new_savings_for_ceiling # Spending ceiling: maintain proportional savings
+
+        sp_floor = previous_deltas.spending # Spending floor: maintain absolute spending
+
+        sp = sp_ceiling * (1 - actual_increase_savings_weight) + sp_floor * actual_increase_savings_weight
+
+        sp_clamped = min(sp, deltas.total_net_income)
+
+        return deltas.update_spending(sp_clamped)
+
+    return increasing_savings_increasing_spending
+
+def get_maxed_or_zeroed_out(x : float, c_m : float, end_region_width: float):
+    """
+    Gets c(x), where it's assumed 0 < x < 1, and c(x) is a piecewise function such that:
+     c(x) = c_m for end_region_width < x < (1 - end_region_width),
+            and goes linearly to c(0) = 0 and c(1) = 1 at the ends
+    """
+
+    if x <= end_region_width:
+        return x * (c_m / end_region_width)
+    elif x < (1 - end_region_width):
+        return c_m
+    else:
+        x_ = x - 1
+        c_ = x_ * ((1 - c_m) / end_region_width)
+        return c_ + 1
--- tests/test_spending_rules.py
+++ tests/test_spending_rules.py
@@ -1,5 +1,6 @@
 import spending_rules
 import model
+import math
 
 def standard_previous_deltas():
     return model.deltas_state.from_year(1999)
@@ -25,3 +26,15 @@ def test_get_luxury_over_basic_capped_above():
         return deltas.update_gross_salary(55800)
     new_delta = model.get_updated_deltas_from_rules(standard_previous_funds(), standard_previous_deltas().update_spending(50000), [set_salary, rule])
     assert 50220 == new_delta.spending
+
+def test_get_maxed_or_zeroed_out():
+    end_region = 0.1
+    c_m = 0.3
+
+    assert c_m == spending_rules.get_maxed_or_zeroed_out(0.5, c_m, end_region)
+    assert c_m == spending_rules.get_maxed_or_zeroed_out(0.1, c_m, end_region)
+    assert math.isclose(c_m, spending_rules.get_maxed_or_zeroed_out(0.9, c_m, end_region))
+    assert 0 == spending_rules.get_maxed_or_zeroed_out(0, c_m, end_region)
+    assert 1 == spending_rules.get_maxed_or_zeroed_out(1, c_m, end_region)
+    assert c_m / 2 == spending_rules.get_maxed_or_zeroed_out(0.05, c_m, end_region)
+    assert c_m / 2 == spending_rules.get_maxed_or_zeroed_out(0.05, c_m, end_region)
