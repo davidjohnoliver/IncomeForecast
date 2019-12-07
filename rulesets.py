80805f1559fea7d09a30567b340b4cc810a82b97
David Oliver <david.john.oliver@gmail.com>
2019-12-06 22:11:38 -0500

    Add Franklin
    Partially optimizes with the increasing_savings_increasing_spending rule

--- rulesets.py
+++ rulesets.py
@@ -205,3 +205,42 @@ def einstein(
             tfsa_interest_rate
         )
     )
+
+def franklin(
+    salary_compound_rate : float, 
+    salary_plateau : float, 
+    base_spending : float, 
+    increase_savings_weight : float,
+    initial_rrsp_allotment : float, 
+    final_rrsp_allotment : float, 
+    initial_year : int, 
+    year_of_retirement : int, 
+    year_of_death : int,
+    retirement_income : float,
+    rrsp_interest_rate : float,
+    tfsa_interest_rate : float,
+    optimize : solve.Optimizing_Solver
+    ):
+    """
+    Ruleset which uses the increasing_savings_increasing_spending rule for spending, and optimizes RRSP/TFSA split for career savings.
+    """
+    career_length_yrs = year_of_retirement - initial_year
+
+    initial_rrsp_func = optimize.subscribe_optimized_scalar("initial_rrsp", lower_bound=0, upper_bound=1, initial_guess=0.5)
+    final_rrsp_func = optimize.subscribe_optimized_scalar("final_rrsp", lower_bound=0, upper_bound=1, initial_guess=0.5)
+
+    return (
+        ruleset.get_career_rules(
+            salary_rules.get_compound_plateau(salary_compound_rate, salary_plateau),
+            spending_rules.get_increasing_savings_increasing_spending(initial_year, increase_savings_weight),
+            savings_rules.get_simple_linear_func(initial_rrsp_func, final_rrsp_func, initial_year, career_length_yrs, optimize.set_failed),
+            rrsp_interest_rate,
+            tfsa_interest_rate
+        ),
+        ruleset.get_retirement_rules(
+            retirement_income,
+            savings_rules.get_simple_retirement_deduction(year_of_retirement, year_of_death),
+            rrsp_interest_rate,
+            tfsa_interest_rate
+        )
+    )
--- tests/test_rulesets.py
+++ tests/test_rulesets.py
@@ -206,3 +206,46 @@ def test_einstein_runs():
 
     assert 46 == len(simulation.all_deltas)
     assert 46 == len(simulation.all_funds)
+
+def test_franklin_runs():
+    """
+    This is a minimal sanity-test that using the ruleset doesn't throw any errors. It doesn't verify correctness (which isn't really possible for full rulesets).
+    """
+
+    simulation = sim.Simulation()
+    simulation.age_at_retirement = 60
+    simulation.year_of_birth = 1990
+    simulation.initial_year = 2025
+    simulation.age_at_death = 80
+    simulation.savings_at_death = 10000
+    simulation.initial_salary = 40000
+    simulation._initial_savings_rrsp = 5000
+    simulation.initial_savings_tfsa = 600
+
+    optimize = solve.Optimizing_Solver(solve.binary_solver, should_invert=True)
+
+    simulation.set_solver(optimize.solve)
+
+    career_rules, retirement_rules = rulesets.franklin(
+        salary_compound_rate=0.05,
+        salary_plateau=70000,
+        base_spending=30000,
+        increase_savings_weight=0.5,
+        initial_rrsp_allotment=0.5,
+        final_rrsp_allotment=0.5,
+        initial_year=simulation.initial_year,
+        year_of_retirement=simulation.year_of_retirement,
+        year_of_death=simulation.year_of_death,
+        retirement_income=50000,
+        rrsp_interest_rate=0.05,
+        tfsa_interest_rate=0.05,
+        optimize=optimize
+    )
+
+    simulation.set_rules(career_rules)
+    simulation.set_retirement_rules(retirement_rules)
+
+    simulation.run()
+
+    assert 46 == len(simulation.all_deltas)
+    assert 46 == len(simulation.all_funds)
