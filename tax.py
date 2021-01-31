b95c27883594d030dbfa6f659d77a0f5e35fd01b
David Oliver <david.john.oliver@gmail.com>
2021-01-31 06:13:15 -0500

    Update tax rates for 2021
    Note there's currently a bug in CalculConversion's calculator, due to the way it accounts for the progressive decrease in the federal personal amount. Now using TaxTips.ca instead as source of truth.

--- tax.py
+++ tax.py
@@ -15,20 +15,21 @@ class _TaxBracket:
 
 
 _qcBrackets = [
-    _TaxBracket(0, 15269, 0),
-    _TaxBracket(15269.01, 43790, 15),
-    _TaxBracket(43790.01, 87575, 20),
-    _TaxBracket(87575.01, 106555, 24),
-    _TaxBracket(106555.01, 0, 25.75),
+    _TaxBracket(0, 15728, 0),
+    _TaxBracket(15728.01, 45105, 15),
+    _TaxBracket(45105.01, 90200, 20),
+    _TaxBracket(90200.01, 109755, 24),
+    _TaxBracket(109755.01, 0, 25.75),
 ]
 
+federalPersonalAmount = 13808
 _caBrackets = [
-    _TaxBracket(0, 12069, 0),
-    _TaxBracket(12069.01, 47630, 15),
-    _TaxBracket(47630.01, 95259, 20.5),
-    _TaxBracket(95259.01, 147667, 26),
-    _TaxBracket(147667.01, 210371, 29),
-    _TaxBracket(210371.01, 0, 33),
+    _TaxBracket(0, federalPersonalAmount, 0),
+    _TaxBracket(federalPersonalAmount+0.1, 49020, 15),
+    _TaxBracket(49020.01, 98040, 20.5),
+    _TaxBracket(98040.01, 151978, 26),
+    _TaxBracket(151978.01, 216511, 29.32),
+    _TaxBracket(216511.01, 0, 33),
 ]
 
 _qcAbatement = 16.5  # https://www.canada.ca/en/revenue-agency/services/tax/individuals/topics/about-your-tax-return/tax-return/completing-a-tax-return/deductions-credits-expenses/line-440-refundable-quebec-abatement.html
@@ -59,7 +60,7 @@ def get_income_tax(taxable_income: float):
     Returns the total income tax owed for the nominated amount of taxable income (earned in Quebec by a Quebec resident).
     """
 
-    assert datetime.datetime.now().year == 2019 # Marginal tax brackets should be kept up to date.
+    assert datetime.datetime.now().year == 2021 # Marginal tax brackets should be kept up to date.
 
     caTax = get_income_tax_from_brackets(
         taxable_income, _qcAbatement, _caBrackets)
--- tests/test_tax.py
+++ tests/test_tax.py
@@ -3,22 +3,22 @@ import math
 import datetime
 
 def test_get_income_tax():
-    correct_answers = [ #Calculated at http://www.calculconversion.com/income-tax-calculator-quebec.html
+    correct_answers = [ #Calculated at https://www.taxtips.ca/calculators/basic/basic-tax-calculator.htm
         (10000, 0),
-        (20000, 1703.01),
-        (30000, 4455.51),
-        (50000, 10379.85),
-        (70000, 17803.35),
-        (90000, 25323.84),
-        (110000, 34284.61),
-        (150000, 53327.04),
-        (200000, 78309.54),
-        (250000, 104615.65),
+        (20000, 1416),
+        (30000, 4169),
+        (50000, 9964),
+        (70000, 17387),
+        (90000, 24811),
+        (110000, 33580),
+        (150000, 52564),
+        (200000, 77626),
+        (250000, 103771),
     ]
 
-    assert datetime.datetime.now().year == 2019 # These values have an expiry date
+    assert datetime.datetime.now().year == 2021 # These values have an expiry date
 
     for pair in correct_answers:
         expected = pair[1]
         actual = tax.get_income_tax(pair[0])
-        assert math.isclose(expected, actual, abs_tol=0.01)
+        assert math.isclose(expected, actual, abs_tol=2)
