ebe39ed83a9ee738c8ea5532e60a36d5e58c804a
David Oliver <david.john.oliver@gmail.com>
2026-01-01 07:09:34 -0500

    Format ruleset

--- ruleset.py
+++ ruleset.py
@@ -5,7 +5,15 @@ Helper functions for building a ruleset.
 import natural_rules
 import model
 
-def get_career_rules(salary_rule, spending_rule, savings_rule, rrsp_interest_rate: float, tfsa_interest_rate: float, unregistered_interest_rate: float):
+
+def get_career_rules(
+    salary_rule,
+    spending_rule,
+    savings_rule,
+    rrsp_interest_rate: float,
+    tfsa_interest_rate: float,
+    unregistered_interest_rate: float,
+):
     """
     Takes assumption-driven rules as input and returns a full career ruleset including 'natural' rules, correctly ordered.
 
@@ -22,21 +30,27 @@ def get_career_rules(salary_rule, spending_rule, savings_rule, rrsp_interest_rat
     return [
         # No current-year dependencies
         natural_rules.apply_tax_refund,
-        natural_rules.get_calculate_investment_interest(rrsp_interest_rate, tfsa_interest_rate, unregistered_interest_rate),
-
+        natural_rules.get_calculate_investment_interest(
+            rrsp_interest_rate, tfsa_interest_rate, unregistered_interest_rate
+        ),
         # Assume no current-year dependencies
         salary_rule,
-        #Depends on deltas.gross_salary (ie salary_rule)
+        # Depends on deltas.gross_salary (ie salary_rule)
         natural_rules.apply_tax,
-
         # May depend on salary and tax
         spending_rule,
-
         # Depends on pretty much everything else
-        savings_rule
+        savings_rule,
     ]
 
-def get_retirement_rules(retirement_income: float, savings_rule, rrsp_interest_rate: float, tfsa_interest_rate: float, unregistered_interest_rate: float):
+
+def get_retirement_rules(
+    retirement_income: float,
+    savings_rule,
+    rrsp_interest_rate: float,
+    tfsa_interest_rate: float,
+    unregistered_interest_rate: float,
+):
     """
     Returns a retirement ruleset including 'natural' rules, correctly ordered.
 
@@ -50,33 +64,56 @@ def get_retirement_rules(retirement_income: float, savings_rule, rrsp_interest_r
     :return: The retirement ruleset.
     """
 
-    def retirement_spending(deltas: model.deltas_state, previous_funds: model.funds_state, previous_deltas: model.deltas_state):
+    def retirement_spending(
+        deltas: model.deltas_state,
+        previous_funds: model.funds_state,
+        previous_deltas: model.deltas_state,
+    ):
         return deltas.update_spending(retirement_income)
 
     return [
-        retirement_spending, # Spend
+        retirement_spending,  # Spend
         # Skip tax on employment salary since we're not employed
-        natural_rules.apply_tax_refund, # Pay tax on previous year's RRSP deductions and investment interest
-        savings_rule, # Deduct spending from RRSP/TFSA according to rule
-        natural_rules.get_calculate_investment_interest(rrsp_interest_rate, tfsa_interest_rate, unregistered_interest_rate) # Earn interest on remaining savings
+        natural_rules.apply_tax_refund,  # Pay tax on previous year's RRSP deductions and investment interest
+        savings_rule,  # Deduct spending from RRSP/TFSA according to rule
+        natural_rules.get_calculate_investment_interest(
+            rrsp_interest_rate, tfsa_interest_rate, unregistered_interest_rate
+        ),  # Earn interest on remaining savings
     ]
 
-def get_couple_ruleset(partner1_salary_rule, partner2_salary_rule, spending_rule, savings_rule, rrsp_interest_rate: float, tfsa_interest_rate: float, unregistered_interest_rate: float):
-    def ruleset(current_year : int, is_partner1_retired : bool, is_partner2_retired : bool):
+
+def get_couple_ruleset(
+    partner1_salary_rule,
+    partner2_salary_rule,
+    spending_rule,
+    savings_rule,
+    rrsp_interest_rate: float,
+    tfsa_interest_rate: float,
+    unregistered_interest_rate: float,
+):
+    def ruleset(
+        current_year: int, is_partner1_retired: bool, is_partner2_retired: bool
+    ):
         for i in range(1, 3):
             # These rules don't have dependencies and apply both pre- and post-retirement
             # (Tax 'refund' can have either sign and is actually a payment when deducting from the RRSP)
-            yield model.get_couple_rule_from_single_rule(natural_rules.apply_tax_refund, i)
-            yield model.get_couple_rule_from_single_rule(natural_rules.get_calculate_investment_interest(rrsp_interest_rate, tfsa_interest_rate, unregistered_interest_rate), i)
+            yield model.get_couple_rule_from_single_rule(
+                natural_rules.apply_tax_refund, i
+            )
+            yield model.get_couple_rule_from_single_rule(
+                natural_rules.get_calculate_investment_interest(
+                    rrsp_interest_rate, tfsa_interest_rate, unregistered_interest_rate
+                ),
+                i,
+            )
 
         # Those who are working earn income and pay tax
-        if (not is_partner1_retired):
+        if not is_partner1_retired:
             yield model.get_couple_rule_from_single_rule(partner1_salary_rule, 1)
             yield model.get_couple_rule_from_single_rule(natural_rules.apply_tax, 1)
-        if (not is_partner2_retired):
+        if not is_partner2_retired:
             yield model.get_couple_rule_from_single_rule(partner2_salary_rule, 2)
             yield model.get_couple_rule_from_single_rule(natural_rules.apply_tax, 2)
-
 
         yield spending_rule
         yield savings_rule
